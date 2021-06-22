80e276fa378a9a50523a208a923c087cfffccd5a
Ali Shouman <shouman@Alis-MacBook-Pro.local>
2021-06-22 23:01:31 +0200

    added demo subrepo to project

---
+++ demo/src/components/Button.tsx
@@ -0,0 +1,30 @@
+const styles: { [key: string]: React.CSSProperties } = {
+    button: {
+        display: 'inline-block',
+        width: '170px',
+        height: '60px',
+        background: '#EFEFEF',
+        outline: 'none',
+        border: '0',
+        textTransform: 'uppercase',
+    },
+    container: {
+        display: 'block',
+        width: '100%',
+        textAlign: 'center',
+        marginBottom: '30px',
+    },
+};
+interface ButtonProps {
+    type: string,
+    handleClick: () => void
+}
+function Button({ type, handleClick }: ButtonProps) {
+    return (
+        <div style={styles.container}>
+            <button style={styles.button} onClick={handleClick}>{type}</button>
+        </div>
+    );
+}
+
+export default Button;
---
+++ demo/src/container/Counter.tsx
@@ -0,0 +1,58 @@
+import { useAsyncReducerState } from '@bitovi/use-async-reducer-state'
+import Button from '../components/Button'
+import Loader from "react-loader-spinner";
+import {CounterState} from '../types'
+
+const styles: { [key: string]: React.CSSProperties } = {
+    container: {
+        display: 'block',
+        width: '100%',
+        textAlign: 'center',
+        marginTop: '50px',
+    },
+    text: {
+        fontSize: '22px',
+        fontWeight: 'bold'
+    }
+};
+
+const initialState: CounterState = {
+    count: 0,
+};
+function Counter() {
+    const wait = function (time: number) {
+        return new Promise((resolve) => { setTimeout(resolve, time) })
+    }
+    const [state, isProcessing, actions] = useAsyncReducerState(
+        // initial state
+        initialState,
+        // collection of reducer methods
+        {
+            async add(state: CounterState, amountToAdd: number): Promise<CounterState> {
+                await wait(1000)
+                return { ...state, count: state.count + amountToAdd };
+            },
+            async subtract(state: CounterState, amountToSubtract: number): Promise<CounterState> {
+                await wait(1000)
+                return { ...state, count: state.count - amountToSubtract };
+            }
+        }
+    )
+    return (
+        <div>
+            <Button type="Two Steps Forward" handleClick={() => actions.add(2)} />
+            <Button type="One Step Back" handleClick={() => actions.subtract(1)} />
+            <div style={styles.container}>
+                <p style={styles.text}>Steps: {state.count}</p>
+                <p style={styles.text}>{isProcessing ? <Loader
+                    type="Puff"
+                    color="#00BFFF"
+                    height={100}
+                    width={100}
+                /> : "Processing completed"}</p>
+            </div>
+        </div>
+    );
+}
+
+export default Counter;
