88a97157863ccb11452d7fbe3c4db41520ed7ac8
Ali Shouman <shouman@Alis-MacBook-Pro.local>
2021-06-24 21:32:11 +0200

    Added error handling

---
+++ demo/src/components/Modal.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import Popup from 'reactjs-popup';
+const styles: { [key: string]: React.CSSProperties } = {
+    "modal": {
+        "fontSize": "12px"
+    },
+    "header": {
+        "width": "100%",
+        "borderBottom": "1px solid gray",
+        "fontSize": "18px",
+        "textAlign": "center",
+        "padding": "5px"
+    },
+    "content": {
+        "width": "100%",
+        "padding": "10px 5px"
+    },
+    "actions": {
+        "width": "100%",
+        "padding": "10px 5px",
+        "margin": "auto",
+        "textAlign": "center"
+    },
+    "close": {
+        "cursor": "pointer",
+        "position": "absolute",
+        "display": "block",
+        "padding": "2px 5px",
+        "lineHeight": "20px",
+        "right": "-10px",
+        "top": "-10px",
+        "fontSize": "24px",
+        "background": "#ffffff",
+        "borderRadius": "18px",
+        "border": "1px solid #cfcece"
+    }
+}
+function Modal({ message, action }: { message: string, action: () => void }) {
+    return (
+        <Popup open modal nested>
+            <div style={styles.modal}>
+                <div style={styles.header}> The following error has occurred </div>
+                <div style={styles.content}>
+                    {' '}
+                    {message}
+                </div>
+                <div style={styles.actions}>
+                    <button style={styles.button} onClick={() => { action() }}>
+                        Retry action
+                    </button>
+                </div>
+            </div>
+        </Popup>
+    )
+}
+export default Modal
--- demo/src/container/Counter.tsx
+++ demo/src/container/Counter.tsx
@@ -1,8 +1,9 @@
-import { useAsyncReducerState } from '@bitovi/use-async-reducer-state'
+import { useAsyncReducerState } from '../asyncreducer/src/index'
 import Button from '../components/Button'
 import Loader from "react-loader-spinner";
-import {CounterState} from '../types'
-
+import { CounterState } from '../types'
+import { useEffect, useRef } from 'react';
+import Modal from '../components/Modal'
 const styles: { [key: string]: React.CSSProperties } = {
     container: {
         display: 'block',
@@ -23,14 +24,20 @@ function Counter() {
     const wait = function (time: number) {
         return new Promise((resolve) => { setTimeout(resolve, time) })
     }
-    const [state, isProcessing, actions] = useAsyncReducerState(
+    const amount = useRef(2);
+    const [state, isProcessing, actions, error] = useAsyncReducerState(
         // initial state
         initialState,
         // collection of reducer methods
         {
             async add(state: CounterState, amountToAdd: number): Promise<CounterState> {
                 await wait(1000)
-                return { ...state, count: state.count + amountToAdd };
+                // This is just a workaround to throw an error the first time this action is invoked
+                if (amount.current === 2) {
+                    amount.current = 1
+                    throw new Error("Error adding amount with value 2");
+                }
+                return { ...state, count: state.count + amount.current };
             },
             async subtract(state: CounterState, amountToSubtract: number): Promise<CounterState> {
                 await wait(1000)
@@ -40,18 +47,22 @@ function Counter() {
     )
     return (
         <div>
-            <Button type="Two Steps Forward" handleClick={() => actions.add(2)} />
-            <Button type="One Step Back" handleClick={() => actions.subtract(1)} />
-            <div style={styles.container}>
-                <p style={styles.text}>Steps: {state.count}</p>
-                <p style={styles.text}>{isProcessing ? <Loader
-                    type="Puff"
-                    color="#00BFFF"
-                    height={100}
-                    width={100}
-                /> : "Processing completed"}</p>
-            </div>
+            {error ? <Modal message={error.message} action={error.redo} /> :
+                <div>
+                    <Button type="Two Steps Forward" handleClick={() => actions.add(2)} />
+                    <Button type="One Step Back" handleClick={() => actions.subtract(1)} />
+                    <div style={styles.container}>
+                        <p style={styles.text}>Steps: {state.count}</p>
+                        <div style={styles.text}>{isProcessing ? <Loader
+                            type="Puff"
+                            color="#00BFFF"
+                            height={100}
+                            width={100}
+                        /> : "Processing completed"}</div>
+                    </div>
+                </div>}
         </div>
+
     );
 }
 
