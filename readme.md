fbc4047e1cd4b325ca13a3b6f49c80834d9588dc
Ali Shouman <ali@bitovi.com>
2021-07-07 20:32:55 +0200

    Formatting

--- readme.md
+++ readme.md
@@ -1,18 +1,15 @@
 # use-async-reducer-state
 
-`useAsyncStateMethods` is a simple async state mechanism for ReactJS.  It's 
+`useAsyncStateMethods` is a simple async state mechanism for ReactJS. It's
 designed to be used for managing service state data and result in easy-to-test reducer methods.
 
-
-## Use 
-
+## Use
 
 In your component, call `useAsyncReducerstate` with:
 
 - The initial state you want to manage
-- A collection of reducer methods, each to be called with the `state` as the first argument and values 
+- A collection of reducer methods, each to be called with the `state` as the first argument and values
   passed to the `action` method as the second argument.
-
 
 ```js
 const [state, isProcessing, actions] = useAsyncReducerstate(
@@ -32,13 +29,13 @@ const [state, isProcessing, actions] = useAsyncReducerstate(
 
 `useAsyncReducerstate` returns:
 
-- `state` - The latest state. This will initially return the initial state value, then it will return 
+- `state` - The latest state. This will initially return the initial state value, then it will return
   the values returned by the reducer methods.
-- `isProcessing` - `true` if an async action is running, `false` if otherwise.  This can be used to add loading, spinners
+- `isProcessing` - `true` if an async action is running, `false` if otherwise. This can be used to add loading, spinners
   or other UI elements to the page.
 - `actions` - An object of methods that can be used to update the state.
 
-When the user interacts with the page, call the `actions` methods. For example, 
+When the user interacts with the page, call the `actions` methods. For example,
 you might call `actions.add` and `actions.subtract` as follows:
 
 ```jsx
@@ -50,4 +47,3 @@ return <div>
     <p>Steps: {{state.count}}</p>
 </div>
 ```
-
--- src/index.ts
+++ src/index.ts
@@ -1,90 +1,87 @@
-import {useState as reactUseState, useRef} from "react";
+import { useState as reactUseState, useRef } from 'react';
 
 // Given an an object of functions and each function returns a promise, creates a combined type for all return values
-type FunctionPromiseReturnType<T> = T extends { [key: string]: (...args: any[]) => PromiseLike<infer RT> } ? RT: unknown;
+type FunctionPromiseReturnType<T> = T extends { [key: string]: (...args: any[]) => PromiseLike<infer RT> }
+  ? RT
+  : unknown;
 
 // An object of functions of unlimited arguments that return a parameterized type
 interface BaseActions<RetType> {
-    [key: string]:  (...args: any[]) => RetType
+  [key: string]: (...args: any[]) => RetType;
 }
 // Given a parameter type, makes a first argument with that type
-type FunctionForFirstParamType<ParamType> = (arg0: ParamType) => void
+type FunctionForFirstParamType<ParamType> = (arg0: ParamType) => void;
 
-export function useAsyncReducerState
-    <
-        // State of the initial state
-        InitialState, 
-         // Actions is an object of functions where each value is a function
-        Actions extends BaseActions<any>
-    >
-    
-    (
-        initialState: InitialState, 
-        actions : Actions,
-        useState = reactUseState
-    ) 
+export function useAsyncReducerState<
+  // State of the initial state
+  InitialState,
+  // Actions is an object of functions where each value is a function
+  Actions extends BaseActions<any>,
+>(
+  initialState: InitialState,
+  actions: Actions,
+  useState = reactUseState,
+): [
+  // Return state
+  FunctionPromiseReturnType<Actions> | InitialState,
+  // Processing
+  boolean,
+  // Methods
+  // Returns an object, for each key of the passed value ...
+  {
+    [PropertyType in keyof Actions]: FunctionForFirstParamType<
+      // Returns a function whose first argument is
+      // The first parameter type
+      Parameters<
+        // For the specific function for an action
+        Actions[PropertyType]
+      >[1]
+    >;
+  },
+] {
+  const [state, setState] = useState(initialState);
+  const [processing, setProcessing] = useState(false);
 
-    : [
-        // Return state
-        FunctionPromiseReturnType<Actions> | InitialState, 
-        // Processing 
-        boolean,
-        // Methods
-        // Returns an object, for each key of the passed value ...
-        {[PropertyType in keyof Actions]: 
-            // Returns a function whose first argument is 
-            FunctionForFirstParamType< 
-                // The first parameter type
-                Parameters<
-                    // For the specific function for an action
-                    Actions[PropertyType]
-                >[1] 
-            >}
-    ]   
-{
-    
-    const [state, setState] = useState(initialState);
-    const [processing, setProcessing] = useState(false);
+  const isProccessing = useRef(false);
+  const currentState = useRef<any>(state);
 
-    const isProccessing = useRef(false);
-    const currentState = useRef<any>(state);
+  const { current: queue } = useRef<any[]>([]);
 
+  const methods = {} as {
+    [PropertyType in keyof Actions]: FunctionForFirstParamType<Parameters<Actions[PropertyType]>>;
+  };
 
-    const {current: queue} = useRef<any[]>([]);
-
-    const methods = {} as {[PropertyType in keyof Actions]: FunctionForFirstParamType<Parameters<Actions[PropertyType]>>};
-
-    // implementation
-    for(const actionName in actions) {
-        if(Object.prototype.hasOwnProperty.call(actions, actionName)) {
-            methods[actionName] = (...args: any[])=> {
-                queue.push({
-                    actionName,
-                    action: actions[actionName],
-                    args
-                });
-                if(!isProccessing.current) {
-                    isProccessing.current = true;
-                    setProcessing(true);
-                    runNext();
-                }
-            };
+  // implementation
+  for (const actionName in actions) {
+    if (Object.prototype.hasOwnProperty.call(actions, actionName)) {
+      methods[actionName] = (...args: any[]) => {
+        queue.push({
+          actionName,
+          action: actions[actionName],
+          args,
+        });
+        if (!isProccessing.current) {
+          isProccessing.current = true;
+          setProcessing(true);
+          runNext();
         }
+      };
     }
-    
-    function runNext(){
-        const actionAndArgs = queue.shift();
-        if(actionAndArgs !== undefined) {
-            actionAndArgs.action(currentState.current, ...actionAndArgs.args).then((latestState: any)=> {
-                currentState.current = latestState;
-                setState(latestState);
-                runNext();
-            });
-        } else {
-            isProccessing.current = false;
-            setProcessing(false);
-        }
-    }
+  }
 
-    return [state,processing, methods];
+  function runNext() {
+    const actionAndArgs = queue.shift();
+    if (actionAndArgs !== undefined) {
+      actionAndArgs.action(currentState.current, ...actionAndArgs.args).then((latestState: any) => {
+        currentState.current = latestState;
+        setState(latestState);
+        runNext();
+      });
+    } else {
+      isProccessing.current = false;
+      setProcessing(false);
+    }
+  }
+
+  return [state, processing, methods];
 }
