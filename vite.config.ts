902e5256616a247ae8d5bdfad7b9e0b0279e4604
David Nicholas <david_nicholas@apple.com>
2022-07-29 07:59:24 -0700

    add vite ts plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
+import typescript from 'rollup-plugin-typescript2'
 
 export default defineConfig((configEnv) => ({
+  plugins: [typescript()],
   build: {
     lib: {
       entry: resolve(__dirname, 'src/use-simple-reducer.ts'),
