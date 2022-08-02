3321a6a65b18dc1bbc0b679f4a6b76ad471fbc7f
David Nicholas <david_nicholas@apple.com>
2022-08-02 09:20:43 -0700

    update formats

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ export default defineConfig((configEnv) => ({
   plugins: [typescript()],
   build: {
     lib: {
+      formats: ['es', 'cjs', 'umd'],
       entry: resolve(__dirname, 'src/use-simple-reducer.ts'),
       name: 'use-simple-reducer',
       fileName: (format) => `use-simple-reducer.${format}.js`,
